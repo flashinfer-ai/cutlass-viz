69eb3a05f78ad18b565674a1acff38541f967516
Zihao Ye <expye@outlook.com>
2025-04-10 21:21:58 +0000

    upd

--- cutlass_viz/core.py
+++ cutlass_viz/core.py
@@ -1,7 +1,7 @@
 import subprocess
 
 cutlass_include_path = [
-    "/Users/zihaoy/cutlass-layout-visualizer/3rdparty/cutlass/include"
+    "/home/zhye/cutlass-viz/3rdparty/cutlass/include"
 ]
 
 
@@ -9,23 +9,31 @@ def visualize_atom():
     code = r"""
 #include <cute/layout.hpp>
 #include <cute/tensor.hpp>
+#include <cute/atom/copy_traits_sm75.hpp>
+
+using namespace cute;
 
 int main() {
-  Layout s2xh4 = make_layout(make_shape (2,make_shape (2,2)),
-                             make_stride(4,make_stride(2,1)));
+  //Layout s2xh4 = make_layout(make_shape (2,make_shape (2,2)),
+  //                           make_stride(4,make_stride(2,1)));
+  using SrcLayout = Layout<Shape < _32,_128>,
+                           Stride<_128,  _1>>;
 
-  print_layout(s2xh4);
+  print_latex(SrcLayout{});
   return 0;
 }
 """
-    with open("print_atom_latex.cc", "w") as f:
+    with open("print_atom_latex.cu", "w") as f:
         f.write(code)
-    compile_cmd = ["g++", "print_atom_latex.cc", "-std=c++17", "-o", "print_atom_latex"]
+    compile_cmd = ["nvcc", "print_atom_latex.cu", "-std=c++17", "-o", "print_atom_latex"]
     for path in cutlass_include_path:
         compile_cmd.append("-I")
         compile_cmd.append(path)
-    subprocess.run(compile_cmd)
-    subprocess.run(["./print_atom_latex"])
+    subprocess.run(compile_cmd, check=True)
+    with open("1.tex", "w") as f:
+        subprocess.run(["./print_atom_latex"], stdout=f, check=True)
+    latex_command = ["pdflatex", "1.tex"]
+    subprocess.run(latex_command, check=True)
     
 if __name__ == "__main__":
     visualize_atom()
